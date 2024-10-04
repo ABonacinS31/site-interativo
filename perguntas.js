f2a4d7663918cfcd02f07590f51827e89fe58ac8
Ana Beatriz <138138924+ABonacinS31@users.noreply.github.com>
2024-10-04 15:07:25 -0300

    segs

--- perguntas.js
+++ perguntas.js
@@ -1,6 +1,9 @@
 criaCartao(
     'filme',
     'qual é a finalidade de filme?',
-    'Ao oferecer uma narrativa aos seus es'
+    'Ao oferecer uma narrativa aos seus espectadores,o cinema tem como principal objetivo fornecer alto grau de convencimento sobre a história contada',
+     
+     'hghg'
     
+
 )
