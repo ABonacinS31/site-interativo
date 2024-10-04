40fd95b84bb183882f006e2a435606e25344bf65
Ana Beatriz <138138924+ABonacinS31@users.noreply.github.com>
2024-10-04 15:16:11 -0300

    fhtsygkr

--- perguntas.js
+++ perguntas.js
@@ -2,8 +2,9 @@ criaCartao(
     'filme',
     'qual é a finalidade de filme?',
     'Ao oferecer uma narrativa aos seus espectadores,o cinema tem como principal objetivo fornecer alto grau de convencimento sobre a história contada',
-     
-     'hghg'
-    
-
 )
+     
+  criaCartao(
+    'matematica',
+'qual é a raiz quadrada de quarenta nove?',
+
